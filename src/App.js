5a697fdebb702a83d4d825ae92346dfce60d8fd2
Saqibi4213 <mehriamosawer@gmail.com>
2024-09-03 16:30:59 +0430

    Rockets components and files done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
-import './App.css';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
+import { getDataFromServer } from './redux/Rockets/RocketsSlice';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDataFromServer());
+  }, [dispatch]);
   return (
     <div className="App">
-      <h1>hello worldy</h1>
+      <Router>
+        <Navbar />
+        <Rockets />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="myprofile" element={<MyProfile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
